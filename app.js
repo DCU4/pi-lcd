c050433052fee7e368d384c86562e7e0a379c9e0
Dylan Connor <DylanJConnor4@gmail.com>
2022-08-10 23:52:33 -0400

    clean up

--- app.js
+++ app.js
@@ -2,25 +2,26 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const https = require('https')
+const fs = require('fs')
 const io = require('socket.io')(http);
-const cors = require('cors')
-const axios = require('axios')
-const LCD = require('@oawu/lcd1602');
-const sensorTemp = require("node-dht-sensor").promises;
+const cors = require('cors');
 const readPi = require('./readPi');
-let intervalId;
 const repeats = 10;
+const logger = require('morgan');
+const logFile = fs.createWriteStream(__dirname + '/node.log', {flags: 'a'}); //use {flags: 'w'} to open in write mode
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(logger('combined', {stream: logFile}));
 // app.use(express.static(__dirname + '/views')); // html
 app.use(express.static(__dirname + '/public')); // js, css, images
 
+runScript('lights_off.py');
 
 // routes
 app.get('/', (req, res) => {
   if (res.statusCode === 200) {
-    // readPi.read();
+    console.log(req.ip);
     res.sendFile('/views/index.html', {
       root: __dirname
     });
@@ -30,21 +31,6 @@ app.get('/', (req, res) => {
 });
 
 
-// need to "cache" data
-// check if data is the same
-// could have 3 objs to check
-
-app.get('/lcd', (req, res) => {
-  if (res.statusCode === 200) {
-    readPi.read();
-    res.send('on');
-
-  } else {
-    return res.status(404);
-  }
-
-});
-
 
 // websocket
 io.sockets.on('connection', function (socket) {
@@ -52,10 +38,10 @@ io.sockets.on('connection', function (socket) {
   socket.on('light', (data) => {
     console.log('data onlight', data)
     if (data) {
-      socket.emit('light on', data);
+      socket.emit('light', data);
       readPi.read();
     } else {
-      socket.emit('light off', data);
+      socket.emit('light', data);
       readPi.lightsOff();
     }
   });
@@ -77,7 +63,7 @@ function emitScript(socket, msg, script, args) {
   });
 }
 
-function runScript(script, args) {
+function runScript(script, args = '') {
   var exec = require('child_process').exec;
   exec(`python3 "${__dirname}/scripts/${script}" ${args}`, function (error, stdout, stderr) {
     if (error) {
